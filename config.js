cc75ef875d1c8c4fb367053fbf9fed7bb1ba52f6
bonniexinh <ntthuy080902@gmail.com>
2025-01-15 10:52:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "Du an lan 1",
+    introTitle: 'Kính gửi anh Hà Trung Nghĩa',
+    introDesc: `Em có điều muốn nói`,
+    btnIntro: '^^Em nói đi ạ^^',
+    title: 'Em muốn hỏi anh là',
+    desc: 'Anh có đang giận dỗi gì em không? ',
+    btnYes: 'Có ạ',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    question: 'Vậy tại sao bạn lại thái độ với tôi?',
+    btnReply: 'Gửi cho em',
+    reply: ' Tại anh bị dở hơi ạ, anh xin lỗi người yêu anh nhiều ạ',
+    mess: 'Mình biết mà. Mình tạm tha cho bạn đấy nhé',
+    messDesc: 'Tối nay 9h, đi xem Tình người duyên ma với mình nhé ạ.',
+    btnAccept: 'Dạa <3',
+    messLink: 'https://www.facebook.com/bonniesayhii' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
