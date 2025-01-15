8bd514c04a4fd5ace93a64691690db022ec7f4eb
bonniexinh <ntthuy080902@gmail.com>
2025-01-15 11:09:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ const CONFIG = {
     desc: 'Anh có đang giận dỗi gì em không? ',
     btnYes: 'Có ạ',
     btnNo: 'Không nha :3',
-    question: 'Vậy tại sao bạn lại thái độ với tôi?',
+    question: 'Rồi làm sao mà bạn lại thái độ với em?',
     btnReply: 'Gửi cho em',
     reply: ' Tại anh bị dở hơi ạ, anh xin lỗi người yêu anh nhiều ạ',
     mess: 'Mình biết mà. Mình tạm tha cho bạn đấy nhé',
-    messDesc: 'Tối nay 9h, đi xem Tình người duyên ma với mình nhé ạ.',
+    messDesc: 'Tối nay 9h, đi xem Tình người duyên ma với em nhé ạ.',
     btnAccept: 'Dạa <3',
     messLink: 'https://www.facebook.com/bonniesayhii' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
